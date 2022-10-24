b4a4eeead996a37bb8356c15d5ff0f6f79083bfa
Abdul Haseeb <Abdulhasiib@live.com>
2022-10-24 12:35:28 +0530

    Draft Design of stopwatch

---
+++ CSharpIntermediate/CSharpIntermediate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var stopWatch = new StopWatch();
+
+                DateTime start = (DateTime)stopWatch.start();
+
+                //DateTime startagain = (DateTime)stopWatch.start();
+
+                Thread.Sleep(11000);
+
+                DateTime stop = (DateTime)stopWatch.stop();
+
+                TimeSpan timeDuration = stop - start;
+                Console.WriteLine(timeDuration);
+                Console.ReadLine();
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ CSharpIntermediate/CSharpIntermediate/StopWatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate
+{
+    class StopWatch
+    {
+        private DateTime? stopWatchStarted = null;
+        private DateTime stopWatchStop;
+
+        public DateTime? start()
+        {
+            if (stopWatchStarted != null)
+            {
+                throw new InvalidOperationException();
+            }
+            stopWatchStarted = DateTime.Now;
+
+            return stopWatchStarted;
+        }
+
+        public DateTime? stop()
+        {
+            stopWatchStop = DateTime.Now;
+            stopWatchStarted = null;
+
+            return stopWatchStop;
+        }
+    }
+
+}
